093d5ba956103fb117ed90b09e1806f402add398
Rick <ricktang1991@gmail.com>
2020-02-27 14:16:56 -0800

    First init

---
+++ SwiftXcode/FloodFill/floodFill.swift
@@ -0,0 +1,11 @@
+//
+//  floodFill.swift
+//  SwiftXcode
+//
+//  Created by 桑染 on 2020-02-27.
+//  Copyright © 2020 Rick. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ SwiftXcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftXcode
+//
+//  Created by 桑染 on 2020-02-27.
+//  Copyright © 2020 Rick. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
